4d381b063d9e07684b8b2fc4d6c36b6a07a57903
davidgit3000 <davidkarik5@gmail.com>
2024-02-19 12:46:04 -0800

    remove cover image in Event Archive and resize the cover image in Future Events

--- assets/js/event-archive.js
+++ assets/js/event-archive.js
@@ -7,53 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
       const container = document.getElementById("event-archive");
 
       for (let val of data) {
-        let carouselItems = "";
-
-        if (val.images && Array.isArray(val.images)) {
-          val.images.forEach((image, index) => {
-            carouselItems +=
-              index === 0
-                ? `
-                  <div class="carousel-item active">
-                    <img src=${image} class="d-block w-100"/>
-                  </div>
-                `
-                : `
-                  <div class="carousel-item">
-                    <img src=${image} class="d-block w-100"/>
-                  </div>
-                `;
-          });
-        }
-
         container.innerHTML += `
             <div class="col">
               <div class="card text-bg-info shadow">
-                <div id="${val.targetId}-" class="carousel slide" data-bs-ride="carousel">
-                  <div class="carousel-inner">
-                    ${carouselItems}
-                  </div>
-                  <button 
-                    class="carousel-control-prev"
-                    type="button"
-                    data-bs-target="#${val.targetId}-"
-                    data-bs-slide="prev"
-                  >
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                  </button>
-              
-                  <button 
-                    class="carousel-control-next"
-                    type="button"
-                    data-bs-target="#${val.targetId}-"
-                    data-bs-slide="next"
-                  >
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                  </button>
-                </div>
-                
+               
                 <div class="card-body">
                   <div class="accordion" id="accordionContainer">
                     <div class="accordion-item">
@@ -82,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
                           <p class="card-text"><span class="fw-bold">Date:</span> ${val.date}</p>
                           <p class="card-text"><span class="fw-bold">Location:</span> ${val.location}</p>
                           <p class="card-text"><span class="fw-bold">Description:</span> ${val.description}</p>
+                          
                         </div>
                       </div>
                     </div>
